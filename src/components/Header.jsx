90ea9027c11d2608bdff95c385f009f31211962c
HieuVo <votrunghieu332001@gmail.com>
2022-12-18 15:21:09 +0700

    withdraw

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const Header = ({ category, title }) => (
-  <div className=" mb-10">
+
+const Header = ({ category, title }) => {
+  return (
+    <div className=" mb-10">
     <p className="text-lg text-gray-400">{category}</p>
     <div className='flex justify-between'>
       <p className="text-3xl font-extrabold tracking-tight text-slate-900">
         {title}
       </p>
-      <button className='text-xl font-semibold m-2 rounded-lg border-2 text-[#5c96de] border-[#2d78d4] ease-in py-2 px-3 hover:bg-sky-700'>Withdraw</button>
+      <button  className='text-xl font-semibold m-2 rounded-lg border-2 text-[#5c96de] border-[#2d78d4] ease-in py-2 px-3 hover:bg-sky-700'>Withdraw</button>
     </div>
   </div>
-);
+  )
+ 
+};
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
+import {
+  DEX_ABI,
+  DEX_SMART_CONTRACT_ADDRESS,
+} from '../abi';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import {  Button } from "../components";
-
+import { Button } from "../components";
+import { ethers } from 'ethers';
 import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '~/components';
 import { useStateContext } from '../contexts/ContextProvider';
 import Modal from './Modal';
 import { useSelector } from 'react-redux';
 import { userSelector } from '~/modules/user';
+import axios from 'axios';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomCenter">
@@ -30,9 +35,97 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   </TooltipComponent>
 );
 
-const Navbar = () => {
-  const {currentUserLogin, currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
+
+const Navbar = () => {
+  const [isConnect, setIsConnect] = useState(false);
+  const [signerAddress, setSignerAddress] = useState('');
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState('');
+
+  useEffect(() => {
+    console.log('hi')
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      await setProvider(provider);
+    };
+    onLoad();
+  }, [])
+
+  const getSigner = async () => {
+    provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    setSigner(signer);
+    console.log({ signer });
+  };
+
+  const handleConnectMetamask = () => {
+    getSigner(provider);
+  }
+  useEffect(() => {
+    if (signer) getWalletAddress();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [signer]);
+
+  const getWalletAddress = () => {
+    signer.getAddress().then((address) => {
+      setSignerAddress(address);
+    });
+    setIsConnect(true)
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const dex_contract = await new ethers.Contract(
+        DEX_ABI,
+        DEX_SMART_CONTRACT_ADDRESS,
+        provider,
+      );
+      // console.log(contractPremium)
+
+    };
+    onLoad();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const handleWithdraw = async () => {
+    let ABI = ['function withdraw() public payable onlyOwner'];
+    // let ABITEST = ['function updatePrice(uint _newPrice)'];
+
+    let iface = new ethers.utils.Interface(ABI);
+    // let ifacetest = new ethers.utils.Interface(ABITEST);
+
+    let params = [
+      {
+        from: signerAddress,
+        to: DEX_SMART_CONTRACT_ADDRESS,
+        gas: '0x1FBD0', // 30400
+        gasPrice: '0x1BF08EB000', // 10000000000000
+        data: iface.encodeFunctionData('withdraw', []),
+        // data: ifacetest.encodeFunctionData('updatePrice', [1]),
+      },
+    ];
+
+    await window.ethereum.request({ method: 'eth_sendTransaction', params }).then((txhash) => {
+
+      // checkTransactionConfirm(txhash).then((result) => {
+      //   if (result) {
+      //     dispatch(authSlice.actions.saveSmartContractInfo({ ...smartContractInfo, balance: ethChange + smartContractInfo.balance }))
+      //     // setBalance((pre) => pre + ethChange);
+      //     toast.dismiss();
+      //     toast.success('Swap successfully', { icon: '👻' });
+      //   }
+      //   const handleRequestStatus = async () => {
+      //     const statusSwapToken = await axios.get(
+      //       `https://api-goerli.etherscan.io/api?module=transaction&action=getstatus&txhash=${txhash}&apikey=P4UEFZVG1N5ZYMPDKVQI7FFU7AZN742U3E`,
+      //     );
+      //     console.log({ statusSwapToken: statusSwapToken.data });
+      //   };
+      //   setTimeout(handleRequestStatus, 10000);
+      // });
+    });
+  };
+
+  const { currentUserLogin, currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
 
@@ -56,10 +149,10 @@ const Navbar = () => {
   const [isShowModal, setIsShowModal] = useState(false)
 
   const openModal = () => {
-     setIsShowModal(true);
+    setIsShowModal(true);
   }
   const closeModal = () => {
-     setIsShowModal(false);
+    setIsShowModal(false);
   }
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
@@ -76,7 +169,13 @@ const Navbar = () => {
         {currentUser || currentUserLogin ? (
           <>
             {" "}
-            <button className='text-md font-semibold m-1 rounded-lg border-2 text-[#03C9D7] border-[#03C9D7] ease-in py-1 px-2 hover:bg-sky-700 '>Connect Wallet</button>
+            {
+              <button onClick={handleWithdraw} className='text-md font-semibold m-1 rounded-lg border-2 text-[#03C9D7] border-[#03C9D7] ease-in py-1 px-2 hover:bg-sky-700 '>Withdraw</button>
+            }
+            {
+              !isConnect ? <button onClick={handleConnectMetamask} className='text-md font-semibold m-1 rounded-lg border-2 text-[#03C9D7] border-[#03C9D7] ease-in py-1 px-2 hover:bg-sky-700 '>Connect Wallet</button>
+                : <button className='text-md font-semibold m-1 rounded-lg border-2 text-[#03C9D7] border-[#03C9D7] ease-in py-1 px-2 hover:bg-sky-700 '>{signerAddress.slice(0, 15) + '...'}</button>
+            }
             <NavButton
               title="Chat"
               dotColor="#03C9D7"
